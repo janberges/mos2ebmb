33385a6d29329543c334211c16dbaa19806e3d0b
Jan Berges <jan.berges@uni-bremen.de>
2024-05-03 23:28:02 +0200

    Calculate d(z²) and d(x²-y², xy) DOS separately

--- dos.py
+++ dos.py
@@ -19,26 +19,37 @@ E, U = elphmod.dispersion.dispersion_full(el.H, nk, vectors=True)
 
 E = E[:, :, -4:]
 U2 = (abs(U) ** 2)[:, :, [0, 3, 4], -4:].sum(axis=2)
+U2z2 = (abs(U) ** 2)[:, :, 0, -4:]
 
 e = np.empty((nk, nk, 2))
+weight = np.empty((nk, nk, 2))
 
 for k1 in range(nk):
     for k2 in range(nk):
-        e[k1, k2, :] = E[k1, k2, sorted(np.argsort(U2[k1, k2])[-2:])]
+        indices = sorted(np.argsort(U2[k1, k2])[-2:])
+        e[k1, k2, :] = E[k1, k2, indices]
+        weight[k1, k2, :] = U2z2[k1, k2, indices] / U2[k1, k2, indices]
 
 e -= mu
 
 w = np.array(list(storylines.multiples(e.min() - dw, e.max() + dw, dw)))
 
 DOS = 0
+DOSz2 = 0
 
 for n in range(2):
     DOS = DOS + elphmod.dos.hexDOS(e[:, :, n])(w)
+    DOSz2 = DOSz2 + elphmod.dos.hexa2F(e[:, :, n], weight[:, :, n])(w)
 
 if comm.rank == 0:
     with open('dos.dat', 'w') as data:
         for iw in range(len(w)):
             data.write('%6.3f %9.6f\n' % (w[iw], DOS[iw]))
+
+    with open('dos2.dat', 'w') as data:
+        for iw in range(len(w)):
+            data.write('%6.3f %9.6f %9.6f\n'
+                % (w[iw], DOSz2[iw], DOS[iw] - DOSz2[iw]))
 
     if plot_3d or plot_dos:
         import matplotlib.pyplot as plt
@@ -58,4 +69,5 @@ if comm.rank == 0:
 
     if plot_dos:
         plt.plot(w, DOS)
+        plt.plot(w, DOSz2)
         plt.show()
