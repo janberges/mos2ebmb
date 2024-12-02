5b2bc6d0b0c0154e0d6fcf98ac67b1a69fd06617
Jan Berges <jan.berges@uni-bremen.de>
2024-12-02 12:06:19 +0100

    Proodread script to plot DOS

--- plot_dos.py
+++ plot_dos.py
@@ -6,13 +6,12 @@ import numpy as np
 w, DOS = np.loadtxt('dos.dat', skiprows=1).T
 w, DOS1, DOS2 = np.loadtxt('dos2.dat', skiprows=1).T
 
-plt.plot(w, DOS, label='total')
-plt.plot(w, DOS1, label='$d_{z^2}$')
-plt.plot(w, DOS2, label='$d_{x^2 - y^2, x y}$')
+plt.plot(w, DOS, color='royalblue', label='total')
+plt.plot(w, DOS1, color='chocolate', label='$d_{z^2}$')
+plt.plot(w, DOS2, color='gold', label='$d_{x^2 - y^2, x y}$')
 plt.plot(w, DOS1 + DOS2, 'k--', label='sum')
 
-plt.xlabel('Energy (eV)')
 plt.ylabel('Density of states (1/eV)')
+plt.xlabel('Energy (eV)')
 plt.legend()
-
 plt.show()
