9868d949beff3d9a623868d77fe53b0b6a7f706f
Jan Berges <jan.berges@uni-bremen.de>
2024-12-02 12:04:24 +0100

    Proofread script to plot band structure

--- plot_el.py
+++ plot_el.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python3
 
 import elphmod
+import matplotlib.patches as pat
 import matplotlib.pyplot as plt
-import numpy as np
-
-comm = elphmod.MPI.comm
-
-colors = ['red'] + 2 * ['blue'] + ['orange', 'yellow'] + 6 * ['green']
 
 el = elphmod.el.Model('mos2')
-mu = elphmod.el.read_Fermi_level('sc.out')
 
 path = 'GMKG'
 k, x, corners = elphmod.bravais.path(path, ibrav=4, N=150)
@@ -17,18 +12,26 @@ k, x, corners = elphmod.bravais.path(path, ibrav=4, N=150)
 e, U, order = elphmod.dispersion.dispersion(el.H, k,
     vectors=True, order=True)
 
-e -= mu
+if elphmod.MPI.comm.rank != 0:
+    raise SystemExit
 
-plt.ylabel('Electron energy (eV)')
-plt.xlabel('Wave vector')
-
-plt.xticks(x[corners], path)
+colors = ['chocolate'] + 2 * ['royalblue'] + 2 * ['gold'] + 6 * ['lightblue']
 
 for n in range(el.size):
-    fatbands = elphmod.plot.compline(x, e[:, n],
-        0.1 * (U[:, :, n] * U[:, :, n].conj()).real)
+    fatbands = elphmod.plot.compline(x, e[:, n], 0.1 * abs(U[:, :, n]) ** 2)
 
     for fatband, color in zip(fatbands, colors):
         plt.fill(*fatband, color=color, linewidth=0.0)
+
+plt.ylabel('Electron energy (eV)')
+plt.xlabel('Wave vector')
+plt.xticks(x[corners], path)
+
+plt.legend(handles=[
+    pat.Patch(color=colors[0], label='Mo-$d_{z^2}$'),
+    pat.Patch(color=colors[3], label='Mo-$d_{x^2 - y^2, x y}$'),
+    pat.Patch(color=colors[1], label='Mo-$d_{x z, y z}$'),
+    pat.Patch(color=colors[5], label='S-$p$'),
+])
 
 plt.show()
